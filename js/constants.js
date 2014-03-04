7f8b875cb5d5da367fd1e62ad40305b9a2f52976
zoghal <saleh.souzanchi@gmail.com>
2014-03-04 05:07:56 +0330

    update

---
+++ js/constants.js
@@ -0,0 +1,255 @@
+var GRUMPIES = {
+	'short': {
+		'text': "لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید.",
+		'columns': [ // single column
+			{
+				sizes: [72, 60, 48, 36, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12],
+				innerblock: 'div'
+			}
+		],
+		'tab': '#headlines'
+	},
+
+	'long': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [ // two columns
+			{sizes: [20, 19, 18, 17], innerblock: '.textsettingCol1'},
+			{sizes: [16, 15, 14, 13, 12, 11, 10], innerblock: '.textsettingCol2'}
+		],
+		'tab': '#text'
+	},
+
+	'lowercaseShort': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [
+			{sizes: [72, 60, 48, 36, 30, 24, 18], innerblock: 'div:first'}
+		],
+		'tab': '#lowercases'
+	},
+
+	'lowercaseLong': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [
+			{sizes: [16, 15, 14], innerblock: '.textsettingCol1'},
+			{sizes: [13, 12, 11, 10], innerblock: '.textsettingCol2'}
+		],
+		'tab': '#lowercases'
+	},
+
+	'adhesionShort': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر",
+		'columns': [
+			{sizes: [72, 60, 48, 36, 30, 24, 18], innerblock: 'div:first'}
+		],
+		'tab': '#adhesion'
+	},
+
+	'adhesionLong': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [
+			{sizes: [16, 15, 14], innerblock: '.textsettingCol1'},
+			{sizes: [13, 12, 11, 10], innerblock: '.textsettingCol2'}
+		],
+		'tab': '#adhesion'
+	},
+
+	'hamburgefonstivShort': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [
+			{sizes: [72, 60, 48, 36, 30, 24, 18], innerblock: 'div:first'}
+		],
+		'tab': '#hamburgefonstiv'
+	},
+
+	'hamburgefonstivLong': {
+		'text': " لورم ایپسوم متن ساختگی به سادگی از صنعت شناخته چاپ استاندارد و وب سایت حروفچینی است. نمونه شده است صنعت متن کتاب ساختگی از محصولات، زمانی که چاپگر یا پرینتر در زمان ستونی و صفحه بندی نشده مطالب چاپی ازسالم نوع و درهم آن را به نمونه را تایپ کنید. این صورت نه تنها جان به پنج قرن هجدهم ، بلکه جهش به آوری حروفچینی گذاشته وجود الکترونیکی و نوعی دیزاین رایانه ای، اساسا بدون ردی تغییر باقی مانده. آن را در فوتبال با انتشار ورق حاوی شهریار معابر فکری هکرها پزشک محبوبیت بود، و اخیرا فکری با نرم افزار هکر را نشر شناخت هیت من رومیزی همانند از جمله نسخه های دیگران بوده است راکه در پی فرهنگ پس از آن و می خواهد به آن را دارند، تنها به استفاده دلیل آن است.. 123456789 ۱۲۳۴۵۶۷۸۹۰",
+		'columns': [
+			{sizes: [16, 15, 14], innerblock: '.textsettingCol1'},
+			{sizes: [13, 12, 11, 10], innerblock: '.textsettingCol2'}
+		],
+		'tab': '#hamburgefonstiv'
+	},
+
+	'caps': {
+		'text': "Arrowroot Barlety Chervil Dumpling Endine Flaxseed Garbanzo Hijiki Ishtu Jicama Kale Lycheen Marjoram Nectarine Oxtail Pizza Quinoa Roquefort Squash Tofu Uppuma Vanilla Wheat Xergis Yogurt Zweiback 0 1 2 3 4 5 6 7 8 9 ! ?",
+		'columns': [
+			{sizes: [60, 48, 36, 30, 28, 24, 20, 18, 16, 14, 13], innerblock: 'div'}
+		],
+		'tab': '#caps'
+	},
+
+	'allcaps': {
+		'text': "ARROWROOT BARLETY CHERVIL DUMPLING ENDINE FLAXSEED GARBANZO HIJIKI ISHTU JICAMA KALE LYCHEEN MARJORAM NECTARINE OXTAIL PIZZA QUINOA ROQUEFORT SQUASH TOFU UPPUMA VANILLA WHEAT XERGIS YOGURT ZWEIBACK 0 1 2 3 4 5 6 7 8 9 ! ?",
+		'columns': [
+			{sizes: [60, 48, 36, 30, 28, 24, 20, 18, 16, 14, 13], innerblock: 'div'}
+		],
+		'tab': '#allcaps'
+	}
+};
+
+var hintsCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var hintsLower = "abcdefghijklmnopqrstuvwxyz.:;,";
+var hintsNumbers= "1234567890 @ &amp;!?#$€%";
+
+
+function eventTextLineChange(e) {
+	var $this = $(this);
+	bulkChangeTextForTab(e.data.grumpy, $this.text(), $this);
+}
+
+
+function onblur() {
+	var $this = $(this);
+	var text = $this.html();
+	if ($this.data('enter') !== text) {
+		$this.data('enter', text);
+		$this.trigger({type: 'change', action : 'save'});
+	}
+	return $this;
+}
+
+
+function onkeyup() {
+	var $this = $(this);
+	var text = $this.html();
+	if ($this.data('before') !== text) {
+		$this.data('before', text);
+		$this.trigger({type: 'change', action : 'update'});
+	}
+	return $this;
+}
+
+
+function onfocus() {
+	var $this = $(this);
+	$this.data('enter', $this.html());
+	$this.data('before', $this.html());
+	return $this;
+}
+
+
+function setColumnTemplate(container, grumpy) {
+	for (var k = 0; k < grumpy.columns.length; k++) {
+
+		var sizes = grumpy.columns[k].sizes;
+		var block = container.find(grumpy.columns[k].innerblock);
+		for (var i = 0; i < sizes.length; i++) {
+			fontsize = sizes[i].toString();
+			block.append($('<p>').addClass('sizelabel').text(fontsize + 'px'));
+
+			var textline = $('<p>').addClass('textline')
+				.css('font-size', fontsize + 'px')
+				.attr('contenteditable', true)
+				.text(grumpy.text)
+				.on('focus', onfocus)
+				.on('keyup paste', onkeyup)
+				.on('blur', onblur)
+				.on('change', {'grumpy': grumpy}, eventTextLineChange);
+
+			block.append(textline);
+			//block.append($('<p>&nbsp;</p>'));
+		}
+	}
+}
+
+function setColumnTemplate2(container, grumpy) {
+	for (var k = 0; k < grumpy.columns.length; k++) {
+
+		var sizes = grumpy.columns[k].sizes;
+		var block = container.find(grumpy.columns[k].innerblock);
+		for (var i = 0; i < sizes.length; i++) {
+			fontsize = sizes[i].toString();
+			block.append($('<p>').addClass('sizelabel').text(fontsize + 'px'));
+
+			var textline = $('<p>').addClass('textline')
+				.css('font-size', fontsize + 'px')
+				.attr('contenteditable', true)
+				.text(grumpy.text)
+				.on('focus', onfocus)
+				.on('keyup paste', onkeyup)
+				.on('blur', onblur)
+				.on('change', {'grumpy': grumpy}, eventTextLineChange);
+
+			block.append(textline);
+			block.append($('<p>&nbsp;</p>'));
+		}
+	}
+}
+
+
+function setSplitSingleToDual(container, grumpies_short, grumpies_long) {
+	setColumnTemplate(container, grumpies_short);
+	setColumnTemplate(container, grumpies_long);
+}
+
+
+function bulkChangeTextForTab(grumpy, value, except) {
+	for (var i = 0; i < grumpy.columns.length; i++) {
+		$(grumpy.tab).find(grumpy.columns[i].innerblock).find('.textline').not(except).text(value);
+	}
+}
+
+
+function prepareAndShowFontLayout() {
+
+	setColumnTemplate($('#headlines'), GRUMPIES.short);
+
+	setColumnTemplate2($('#text'), GRUMPIES.long);
+
+	var $lowercases = $('#lowercases');
+	setColumnTemplate($lowercases, GRUMPIES.lowercaseShort);
+	setColumnTemplate2($lowercases, GRUMPIES.lowercaseLong);
+
+	var $adhesion = $('#adhesion');
+	setColumnTemplate($adhesion, GRUMPIES.adhesionShort);
+	setColumnTemplate2($adhesion, GRUMPIES.adhesionLong);
+
+	var $hamburgefonstiv = $('#hamburgefonstiv');
+	setColumnTemplate($hamburgefonstiv, GRUMPIES.hamburgefonstivShort);
+	setColumnTemplate2($hamburgefonstiv, GRUMPIES.hamburgefonstivLong);
+
+	var $caps = $('#caps');
+	setColumnTemplate2($caps, GRUMPIES.caps);
+
+	var $allcaps = $('#allcaps');
+	setColumnTemplate2($allcaps, GRUMPIES.allcaps);
+
+	var target = document.createElement('div');
+	target.setAttribute('style','width: 920px;');
+	for (var a = 28; a>8; a--) {
+		var sizelabel = document.createElement('p');
+		sizelabel.setAttribute('class','sizelabel');
+		sizelabel.textContent = a + 'px';
+		target.appendChild(sizelabel);
+
+		var hintslower = document.createElement('p');
+		hintslower.setAttribute('contenteditable','true');
+		hintslower.setAttribute('class','hints-lower');
+		hintslower.setAttribute('style','font-size: ' + a + 'px');
+		target.appendChild(hintslower);
+
+		var hintscaps = document.createElement('p');
+		hintscaps.setAttribute('contenteditable','true');
+		hintscaps.setAttribute('class','hints-caps');
+		hintscaps.setAttribute('style','font-size: ' + a + 'px');
+		target.appendChild(hintscaps);
+
+		var hintsnumbers = document.createElement('p');
+		hintsnumbers.setAttribute('contenteditable','true');
+		hintsnumbers.setAttribute('class','hints-numbers');
+		hintsnumbers.setAttribute('style','font-size: ' + a + 'px');
+		target.appendChild(hintsnumbers);
+
+		var hintsend = document.createElement('p');
+		hintsend.innerHTML = '&nbsp;';
+		target.appendChild(hintsend);
+	};
+	document.getElementById('hinting').appendChild(target);
+
+	var hints_caps = $('.hints-caps');
+	var hints_numbers = $('.hints-numbers');
+	var hints_lower = $('.hints-lower');
+	hints_lower.html(hints_lower.html() + hintsLower);
+	hints_caps.html(hints_caps.html() + hintsCaps);
+	hints_numbers.html(hints_numbers.html() + hintsNumbers);
+}
