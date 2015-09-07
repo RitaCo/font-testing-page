362c40714d20b5d6db4f5c7c8c602f494d5a9c95
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2015-09-07 12:36:26 +0430

    1.5.1

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 var GRUMPIES = {
 	'short': {
-		'text': "یک فونت استاندارد باید هم از نظر طراحی و هم اجرای نرم‌افزاری کاملاً استاندارد باشد.",
+		'text': "یک فونت (Font) استاندارد standard باید هم از نظر طراحی {design} و هم اجرای نرم‌افزاری [Software] کاملاً استاندارد باشد.",
 		'columns': [ // single column
 			{
 				sizes: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72],
@@ -17,7 +17,7 @@ var GRUMPIES = {
 			{sizes: [28, 26, 24, 22, 20], innerblock: '.textsettingCol2'},
 			{sizes: [19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  6], innerblock: '.textsettingCol3'}
 		],
-		'tab': '#text'
+		'tab': '#persian-text-paragraf'
 	},
 
 	'longArabic': {
@@ -27,7 +27,7 @@ var GRUMPIES = {
 			{sizes: [28, 26, 24, 22, 20], innerblock: '.textsettingCol2'},
 			{sizes: [19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  6], innerblock: '.textsettingCol3'}
 		],
-		'tab': '#text'
+		'tab': '#arabic-text-paragraf'
 	},
 
 	'lowercaseLong': {
@@ -181,7 +181,10 @@ function setSplitSingleToDual(container, grumpies_short, grumpies_long) {
 
 function bulkChangeTextForTab(grumpy, value, except) {
 	for (var i = 0; i < grumpy.columns.length; i++) {
-		$(grumpy.tab).find(grumpy.columns[i].innerblock).find('.textline').not(except).text(value);
+		var x = $(grumpy.tab).find(grumpy.columns[i].innerblock);
+		var z = x.find('.textline').not(except);
+		
+		z.text(value);
 	}
 }
 
